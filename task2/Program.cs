5278437b2490a9bf843b8fe4913011f4aa6f33fc
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-09-03 21:53:26 +0500

    new task

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите три числа, ");
+Console.WriteLine("какое из них имеет максимальное значение.");
+Console.Write("Введите первое число нажмите ENTER:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число нажмите ENTER:");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число нажмите ENTER:");
+int num_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num_1;
+
+if (num_2 > max)
+{
+    max = num_2;
+}
+
+if (num_3 > max) ;
+{
+    max = num_3;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел ->" + max);
---
+++ task2/bin/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+num = Convert.ToInt32(Consol.ReadLine());
+
+if (num % 2 == 1)
+{
+    Console.WriteLine("Число" + num + "является: НЕЧЕТНЫМ");
+}
+else
+{
+    Console.WriteLine("Число" + num + "является: ЧЕТНЫМ");
+}
